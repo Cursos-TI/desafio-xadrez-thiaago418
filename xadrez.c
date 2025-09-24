cbd927e0ea6c894a3f90d226185c8b5f3fe4acba
thiaago418 <thiaago418@gmail.com>
2025-09-24 00:31:51 +0000

    Loops Aninhados

--- xadrez.c
+++ xadrez.c
@@ -1,37 +1,82 @@
 #include <stdio.h>
 
+void moverTorre(int casas_restantes) {
+    if (casas_restantes == 0) {
+        return;
+    }
+
+    printf("Direita\n");
+
+    moverTorre(casas_restantes - 1);
+}
+
+void moverBispo(int casas_restantes) {
+    if (casas_restantes == 0) {
+        return;
+    }
+
+    printf("Cima, Direita\n");
+
+    moverBispo(casas_restantes - 1);
+}
+
+void moverRainha(int movimentos_restantes) {
+    if (movimentos_restantes == 0) {
+        return;
+    }
+
+    printf("Esquerda\n");
+
+    moverRainha(movimentos_restantes - 1);
+}
+
 int main() {
-    int i;
-    int casas_andadas = 0;
-    int movimentos = 0;
-
     printf("--- Movimento da Torre ---\n");
-    for (i = 1; i <= 5; i++) {
-        printf("Casa %d: Direita\n", i);
-    }
+    moverTorre(5);
 
-    printf("\n--- Movimento do Bispo ---\n");
-    while (casas_andadas < 5) {
-        casas_andadas++;
-        printf("Casa %d: Cima, Direita\n", casas_andadas);
-    }
+    printf("\n");
 
-    printf("\n--- Movimento da Rainha ---\n");
-    do {
-        movimentos++;
-        printf("Casa %d: Esquerda\n", movimentos);
-    } while (movimentos < 8);
+    printf("--- Movimento do Bispo ---\n");
+    moverBispo(5);
 
-    printf("\n--- Movimento do Cavalo ---\n");
-    int ic = 0;
-    int icc;
+    printf("\n");
+    
+    printf("--- Movimento da Rainha ---\n");
+    moverRainha(8);
 
-    while (ic < 1) {
-        for (icc = 0; icc < 2; icc++) {
-            printf("Baixo\n");
+    printf("\n");
+
+    printf("--- Movimento do Cavalo ---\n");
+    
+    int num_l_moves = 3; 
+
+    for (int i = 0; i < num_l_moves; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Cima\n");
         }
-        printf("Esquerda\n");
-        ic++;
+        
+        printf("Direita\n");
+
+        if (i == 5) {
+            break;
+        }
+
+        if (i == 1) {
+            continue;
+        }
+    }
+
+    printf("\n");
+
+    printf("--- Movimento do Bispo (Loops Aninhados) ---\n");
+    int casas_diagonais = 4;
+    for (int i = 0; i < casas_diagonais; i++) {
+        for (int j = 0; j < 1; j++) {
+            printf("Cima\n");
+        }
+        for (int k = 0; k < 1; k++) {
+            printf("Direita\n");
+        }
     }
 
     return 0;
